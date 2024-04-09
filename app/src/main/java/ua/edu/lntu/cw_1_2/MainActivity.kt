c4e2ae3458a3baaae47b5decb3f80b1a4253708f
DenisQK <denus.nazarovets@gmail.com>
2024-04-09 16:58:45 +0300

    Добавив базову логіку для майбутньої навігації

--- app/src/main/java/ua/edu/lntu/cw_1_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_1_2/MainActivity.kt
@@ -1,15 +1,46 @@
 package ua.edu.lntu.cw_1_2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import ua.edu.lntu.cw_1_2.ui.theme.IPZ_CW_1_2_Nazarovets_DenysTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,5 +62,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
+    val navController = rememberNavController()
 
+    NavHost(navController, startDestination = "SignIn") {
+        composable("SignIn") {
+        }
+        composable("SignUp") {
+        }
+    }
 }
+
+
